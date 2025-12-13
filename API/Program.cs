d014948a30ac0f38cc492ad48f7e8ddbaab2926f
Hendrik Van Dyck <hendrik.vandyck@student.kdg.be>
2025-12-13 20:04:15 +0100

    DaisyUI & Tailwind

--- API/Program.cs
+++ API/Program.cs
@@ -28,6 +28,12 @@ builder.Services.AddIdentityCore<AppUser>(opt =>
 
 var app = builder.Build();
 
+app.UseCors(x => 
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 
 app.UseAuthorization();
 
